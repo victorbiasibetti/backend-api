4d8231cd9b28ebf6255bda63ca51aba7617c04fd
Victor Biasibetti <victorbiasibetti7@gmail.com>
2020-03-25 17:29:14 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors());
+
+app.get('/', (request, response) => {
+  response.status(200).send({
+    msg: 'hello world'
+  })
+})
+
+
+app.listen(4000, () => console.log('server on'));
